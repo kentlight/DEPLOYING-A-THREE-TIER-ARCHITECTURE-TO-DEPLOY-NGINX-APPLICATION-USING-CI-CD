1e3fb4c758e80cf0de24c2f58bb74e5203851a7d
Ubuntu <ubuntu@ip-10-0-13-13.ec2.internal>
2024-04-13 21:25:03 +0000

    “added”

---
+++ installer.sh
@@ -0,0 +1,37 @@
+# update the ubuntu server
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+
+# install unzip
+
+sudo apt-get install unzip
+
+# install terraform
+
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt-get update && sudo apt-get install terraform
+
+# install aws cli
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+sudo ./aws/install --update
+
+
+
